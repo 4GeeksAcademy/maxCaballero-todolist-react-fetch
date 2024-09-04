339a9a0fb4c64f1da0c554c45e1cfdfba2323cd9
Christian Caballero <xtian.cr78@gmail.com>
2024-09-04 17:37:35 +0000

    finalizado todolist con react y fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,49 @@
-import React, { useState } from "react";
-import ".././styles/home.css";
+import React, { useState, useEffect } from "react";
+import "../../styles/home.css";
 
 // Create your first component
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [tareas, setTareas] = useState([]);
 
+  useEffect(() => {
+    fetch("https://playground.4geeks.com/tareas/user/alesanchezr", {
+      method: "PUT",
+      body: JSON.stringify(tareas),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        console.log(resp.ok); // Será true si la respuesta es exitosa
+        console.log(resp.status); // El código de estado 200, 300, 400, etc.
+        //console.log(resp.text()); // Intentará devolver el resultado exacto como string
+        return resp.text(); // Retornará una promesa donde puedes usar .then para seguir con la lógica
+      })
+      .then((text) => {
+        console.log(text);
+        return JSON.parse(text);
+      })
+      .then((data) => {
+        // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+      })
+      .catch((error) => {
+        // Manejo de errores
+        console.log(error);
+      });
+  }, [tareas]);
+
   const handleKeyPress = (e) => {
     if (e.key === "Enter" && inputValue.trim()) {
       // Ensure input is not empty
-      setTareas([...tareas, inputValue]); // Use spread operator for better readability
+      setTareas([...tareas, inputValue]);
       setInputValue("");
     }
   };
 
   const handleDelete = (index) => {
-    setTareas(tareas.filter((_, idx) => idx !== index)); // Improved clarity
+    setTareas(tareas.filter((_, idx) => idx !== index));
   };
 
   return (
